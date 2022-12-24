b813e87c1c8e5c45b2b91642d68583d62ba61642
koteyka5000 <romacat5000@gmail.com>
2022-12-24 22:15:50 +0300

    v3 Можно получить текст и ключ раздельно

--- koteyka_encryption.py
+++ koteyka_encryption.py
@@ -23,7 +23,7 @@ alphabet = ["ю", "в", "з", "й", "м", "с", "т", "у", "ь", "ж", "л",
 "О", "Ц", "Ч", "Ш", "Щ", "Р", "Н", "А", "П", "Д", "Е"
 ]
 
-def encrypt(txt):
+def encrypt(txt:str, split:bool=False):
     encrypted = ''
     move_keys = []
 
@@ -38,27 +38,31 @@ def encrypt(txt):
     for key in move_keys:                  #  | Записываем каждую цифру как букву с этим индексом, чтобы получить букву из числа
         move_keys_letters += alphabet[key] # / 
 
-    print(f'encrypted = {encrypted}')
-    print(f'move_keys_letters = {move_keys_letters}')
-
     new_encrypted = ''  
     for letter in encrypted:                       # \
         new_encrypted += letter                    #  | Добавляем через каждую букву случайную букву, чтобы усложнить иотговую комбинацию
         new_encrypted += alphabet[randint(0, 33)]  # /
 
     encrypted_ready_send = new_encrypted + move_keys_letters # Итогая строка это соединение шифра и ключа к нему
-    return encrypted_ready_send 
+    if not split:
+        return encrypted_ready_send
+    else:
+        return new_encrypted, move_keys_letters
+    
 
-def decrypt(encrypted):
-    print('==================')
-    encrypted_ready = encrypted
-    enc_len = len(encrypted_ready) # Длинна шифра будет 2\3, а ключ 1\3, тк мы добавляли через каждую букву случайную букву, получается
-    enc_len = enc_len // 3         # что длинна шифра будет в 2 раза больше, чем ключ к шифру           (Тут получаем 1\3 длинны строки)
-    encrypted = encrypted_ready[:enc_len + enc_len][::2]  # Срезаем первые две части, получая зашифрованый текст без ключа, потом убираем
-                                                          # каждую вторую букву, чтобы получить исходный шифр
-    move_keys_letters = encrypted_ready[enc_len:]    # срезаем 2\3 c конца (я хз как по другому) 
-    move_keys_letters = move_keys_letters[enc_len:]  # срезаем еще половину (1\2), получая 1\3, где и находится ключ
-    print(f'{encrypted}    :    {move_keys_letters}')
+def decrypt(encrypted: str, key:str=None):
+    if key:
+        encrypted = encrypted[::2]
+        move_keys_letters = key
+    else:
+        encrypted_ready = encrypted
+        enc_len = len(encrypted_ready) # Длинна шифра будет 2\3, а ключ 1\3, тк мы добавляли через каждую букву случайную букву, получается
+        enc_len = enc_len // 3         # что длинна шифра будет в 2 раза больше, чем ключ к шифру           (Тут получаем 1\3 длинны строки)
+        encrypted = encrypted_ready[:enc_len + enc_len][::2]  # Срезаем первые две части, получая зашифрованый текст без ключа, потом убираем
+                                                            # каждую вторую букву, чтобы получить исходный шифр
+        move_keys_letters = encrypted_ready[enc_len:]    # срезаем 2\3 c конца (я хз как по другому) 
+        move_keys_letters = move_keys_letters[enc_len:]  # срезаем еще половину (1\2), получая 1\3, где и находится ключ
+    print(f'{encrypted}    <txt | key>    {move_keys_letters}')
 
     decrypted = ''
     counter = 0
@@ -78,3 +82,12 @@ print(f'Зашифрованное сообщение: {encrypted}')
 
 decrypted = decrypt(encrypted)
 print(f'Расшифрованное сообщение: {decrypted}')
+
+print('======================================')
+print('Можно также получить текст и ключ по отдельности:\n')
+
+encrypted_txt, encrypted_key = encrypt(txt, True)
+print(f'{encrypted_txt} : {encrypted_key}')
+
+decrypted = decrypt(encrypted_txt, encrypted_key)
+print(decrypted)
